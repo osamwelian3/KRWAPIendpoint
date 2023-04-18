faea90844416acc428d3d3da6cbb153c3cfcc717
Samuel Ian <osamwelian3@gmail.com>
2023-04-18 19:40:14 +0300

    register mpesa_daraja models to admin site

--- mpesa_daraja/admin.py
+++ mpesa_daraja/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Token)
+admin.site.register(PendingTransaction)
+admin.site.register(CompleteTransaction)
+admin.site.register(Confirmation)
