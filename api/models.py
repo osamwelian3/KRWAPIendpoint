108e72880e0b945f933b255624413332ed4a5c62
Samuel Ian <osamwelian3@gmail.com>
2023-04-19 11:05:06 +0300

    Add Profile model and its serializer

--- api/models.py
+++ api/models.py
@@ -1,14 +1,7 @@
 from django.db import models
-from django.conf.global_settings import AUTH_USER_MODEL
-
-User = AUTH_USER_MODEL
+from django.contrib.auth.models import User
 
 # Create your models here.
-# class PendingTransaction(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     transaction_reason = models.CharField(max_length=255)
-#     amount = models.IntegerField()
-#     phone_number = models.CharField(max_length=13)
-
-#     def __str__(self) -> str:
-#         return f'Transaction of {self.amount} for {self.transaction_reason}'
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    phone = models.CharField(max_length=13)
--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,31 @@
 from django.contrib.auth.models import User
+from .models import Profile
 from rest_framework import serializers
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(required=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email', 'profile')
+
+    def create(self, validated_data):
+        """
+        Make necessary modifications as per your requirements
+        """
+        profile_validated_data = dict(validated_data['profile'])
+        print(profile_validated_data)
+        nvd = validated_data.copy()
+        nvd.pop("profile")
+        print(nvd)
+        create = super().create(validated_data=nvd)
+        print(create.id)
+        profile_validated_data.update({'user': create})
+        user_profile = ProfileSerializer.create(ProfileSerializer(), profile_validated_data)
+        # profile, created = Profile.objects.create(profile=user_profile)
+        return create
